866b9f6e794200c0769e66459415734be32f80ee
gaelenotoole <gaelenotoole@gmail.com>
2016-10-10 15:59:05 +0100

    driver

---
+++ src/main/java/Driver.java
@@ -0,0 +1,28 @@
+/**
+ * Created by gaelen on 10/10/2016.
+ */
+public class Driver {
+
+    Student student1 = new Student();
+    Student student2 = new Student();
+    Student student3 = new Student();
+
+    student1.setName("john");
+    student2.setName("Mick");
+    student3.setName("Tim");
+
+    Module module1 = new Module();
+    Module module2 = new Module();
+    Module module3 = new Module();
+
+    module1.setModuleName("Biology");
+    module2.setModuleName("French");
+    module3.setModuleName("Mathematics");
+
+    ProgrammeClasses Course1 = new ProgrammeClasses();
+    ProgrammeClasses Course2 = new ProgrammeClasses();
+
+
+
+
+}
